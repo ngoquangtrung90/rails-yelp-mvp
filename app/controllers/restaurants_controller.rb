89464f10d8d0562acbcc4af7b53bafd872098f55
ngo quang trung <quangtrungxf@gmail.com>
2023-04-29 23:51:23 +0200

    yelp mvp with resources redo to understand ruby on rails

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,44 +3,34 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
 
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
   def new
     @restaurant = Restaurant.new
   end
 
   def create
     @restaurant = Restaurant.new(restaurant_params)
-    puts 'create'
-    if @restaurant.valid?
-      @restaurant.save
+    if @restaurant.save
       redirect_to restaurant_path(@restaurant)
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-  def show
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    # raise
-  end
-
   def edit
-    @restaurant = Restaurant.find(params[:restaurant_id])
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def update
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    if @restaurant.valid?
-      @restaurant.update(restaurant_params)
+    @restaurant = Restaurant.find(params[:id])
+    if @restaurant.update(restaurant_params)
       redirect_to restaurant_path(@restaurant)
     else
       render :edit, status: :unprocessable_entity
     end
-  end
-
-  def destroy
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    @restaurant.destroy
-    redirect_to restaurants_path, status: :see_other
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get 'restaurants', to: 'restaurants#index'
-  get 'restaurants/new', to: 'restaurants#new'
-  get 'restaurants/:restaurant_id', to: 'restaurants#show', as: :restaurant
-  get 'restaurants/:restaurant_id/edit', to: 'restaurants#edit', as: :edit
-  get 'restaurants/:restaurant_id/reviews', to: 'reviews#index', as: :reviews
-  get 'restaurants/:restaurant_id/reviews/new', to: 'reviews#new', as: :review_new
-  post 'restaurants', to: 'restaurants#create'
-  post 'restaurants/:restaurant_id/reviews', to: 'reviews#create', as: :restaurant_reviews
-  patch 'restaurants/:restaurant_id', to: 'restaurants#update'
-  delete 'restaurants/:restaurant_id', to: 'restaurants#destroy', as: :destroy
+  resources :restaurants do
+    resources :reviews, only: [:index, :new, :create]
+  end
 end
